6441fef80311e8169b700a4532dec3d11e388c03
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-14 17:03:21 +0500

    set variable

---
+++ lesson5.ts
@@ -0,0 +1,10 @@
+// let city:string = "Toshkent";
+
+// city = city.toUpperCase();
+// city = city.trim();
+
+let city;
+city = "Toshkent";
+
+// city = (<string>city).trim();
+city = (city as string).trim();
