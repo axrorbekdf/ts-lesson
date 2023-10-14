24603e010008d7dac4fd8c35c37349c8a2669d8a
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-14 16:45:11 +0500

    Var and Let

---
+++ lesson3.js
@@ -0,0 +1,7 @@
+function someFuntion() {
+    for (var i = 0; i < 3; i++) {
+        console.log(i);
+    }
+    console.log("Oxirgi qiymat:" + i);
+}
+someFuntion();
---
+++ lesson3.ts
@@ -0,0 +1,10 @@
+function someFuntion(){
+    for(var i = 0; i < 3; i++){
+        console.log(i);
+    }
+
+    console.log("Oxirgi qiymat:" + i);
+}
+
+
+someFuntion();
