64fc1594322908f727c0ab7662e37df23359107c
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-14 17:43:47 +0500

    Ko'rsatgich funksiyalar

--- lesson6.ts
+++ lesson6.ts
@@ -6,4 +6,5 @@ let log = function(message){
 //     console.log(message);
 // }
 
-let doLog = (message) => console.log(message);
+// let doLog = (message) => console.log(message);
+let doLog = () => console.log(message);
