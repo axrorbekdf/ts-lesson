b4ff6250740aad2023c6f4164ae78d1cd3b4c59f
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-14 16:56:05 +0500

    Typescript variables

--- lesson4.ts
+++ lesson4.ts
@@ -3,3 +3,16 @@ let count = 10;
 // count = "asdasd"; //error
 count = 23; // success
 
+let firstName ="Admin"; // string
+
+let abs; // any - xoxlagan typedagi o'zgaruvchilarni bersak bo'ladi.
+abs = "asdas";
+abs = 21331;
+abs = true;
+// best practice ko'ra o'zgaruvchilarni aniq qilib berish zarur
+// let abs:number
+let abs1:number // string, boolean, 
+
+// massiv
+let m:number[];
+let m1:any[];
