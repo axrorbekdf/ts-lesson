e60cc3a1675d3468d06515d99f3ca2fc74ce7670
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-10-16 10:54:58 +0500

    Xossalar ts

---
+++ lesson12.js
@@ -0,0 +1,40 @@
+var Shape = /** @class */ (function () {
+    function Shape(x, y) {
+        // ixtiyoriy argumantlar
+        this._x = x;
+        this._y = y;
+    }
+    Object.defineProperty(Shape.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("Value cannot be less than 0.");
+            this._x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Shape.prototype, "y", {
+        get: function () {
+            return this._y;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("Value cannot be less than 0.");
+            this._y = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Shape.prototype.draw = function () {
+        console.log(this._x.toString());
+        console.log(this._y.toString());
+    };
+    return Shape;
+}());
+var shape = new Shape(10, 20);
+shape.x = 45;
+shape.y = 55;
+shape.draw();
